50793281f7bf2ece9604bde893c26b2bb507d1e8
Victor <victorahorton@gmail.com>
2019-10-03 21:36:40 -0400

    added config and no image asset

--- src/components/profile/Index.js
+++ src/components/profile/Index.js
@@ -1,15 +1,41 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Image, StyleSheet} from 'react-native';
 import Page from '../common/Page';
+import Theme from '../../config/Theme';
 
-class SettingsScreen extends React.Component {
+class ProfileScreen extends React.Component {
   render() {
     return (
       <Page title="Profile">
-        <Text>Profile!</Text>
+        <View style={styles.topStyle}>
+          <Text style={styles.titleStyle}>User Name</Text>
+          <Image
+            source={require('../../assets/NoImage.png')}
+            style={styles.noImageStyle}
+          />
+        </View>
       </Page>
     );
   }
 }
 
-export default SettingsScreen;
+const styles = StyleSheet.create({
+  topStyle: {
+    height: '50%',
+    backgroundColor: Theme.colors.lightGray,
+    alignItems: 'center',
+  },
+  titleStyle: {
+    textAlign: 'center',
+    marginTop: '15%',
+    marginBottom: 20,
+    fontSize: 24,
+  },
+  noImageStyle: {
+    height: 120,
+    width: 120,
+    borderRadius: 120 / 2,
+  },
+});
+
+export default ProfileScreen;
---
+++ src/config/Theme.js
@@ -0,0 +1,23 @@
+import {Dimensions} from 'react-native';
+
+const deviceWidth = Dimensions.get('window').width;
+const deviceHeight = Dimensions.get('window').height;
+
+const lightGray = '#a8a8a8';
+const lightTurq = '#00e7b3';
+const offBlack = '#111111';
+const lightBlue = '#5ea4ff';
+
+export default {
+  dimensions: {
+    deviceWidth: deviceWidth,
+    deviceHeight: deviceHeight,
+  },
+  colors: {
+    white: '#fff',
+    lightGray: lightGray,
+    lightTurq: lightTurq,
+    offBlack: offBlack,
+    lightBlue: lightBlue,
+  },
+};
