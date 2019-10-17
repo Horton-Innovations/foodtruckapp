c0f538306ca96b9123b91a8b034c0db9ad449758
Victor <victorahorton@gmail.com>
2019-10-16 21:54:45 -0400

    added map and style to map comp

--- src/components/map/Index.js
+++ src/components/map/Index.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import Page from '../common/Page';
+import MapView from 'react-native-maps';
 
 const MapScreen = () => {
   return (
     <Page title="Map">
-      <Text>Map Screen</Text>
+      <View style={styles.container}>
+        <Text>Map Screen</Text>
+        <MapView
+          style={styles.mapContainer}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </View>
     </Page>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  mapContainer: {
+    flex: 1,
+  },
+});
 
 export default MapScreen;
