5ae3d153c5d64658bd3996d26143b0ba66a136b0
Victor <victorahorton@gmail.com>
2019-10-17 16:56:50 -0400

    added package and rest of native code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, {Component} from 'react';
 import TabNavigator from './navigation';
+import SplashScreen from 'react-native-splash-screen';
 
 class App extends Component {
   state = {Loading: true};
+
+  componentDidMount() {
+    // do stuff while splash screen is shown
+    // After having done stuff (such as async tasks) hide the splash screen
+    SplashScreen.hide();
+  }
 
   render() {
     return <TabNavigator />;
